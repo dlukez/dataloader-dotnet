df0be9c2b1779cd2473c05c38e83a93f76026bbe
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-01-15 12:00:22 +1100

    revert myget.bat

--- MyGet.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-dotnet restore ^
-&& dotnet build --configuration Release src/DataLoader ^
-&& dotnet test  --configuration Release test/DataLoader.Tests ^
-&& dotnet pack  --configuration Release --no-build --version-suffix "%PrereleaseTag%" src/DataLoader
-
-exit /b
