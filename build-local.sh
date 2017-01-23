fd5e68e9b2e58fbedfa14027958391aee28789fa
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-01-23 14:35:51 +1100

    No longer need local build and publish now that the GraphQL samples are in this repo

--- build-local.sh
+++
@@ -1,7 +0,0 @@
-PrereleaseTag=$1
-Configuration=Release
-
-dotnet restore
-dotnet build src/DataLoader/ --configuration $Configuration
-dotnet test test/DataLoader.Tests/ --configuration $Configuration
-dotnet pack src/DataLoader/ --configuration $Configuration --version-suffix $PrereleaseTag --output ~/.nuget/feed/
