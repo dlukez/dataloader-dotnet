b4f0f8cf307a4fcc45446e7f2716219f75046b95
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-01-27 14:34:19 +1100

    Begin experiment: sync context to enable breaking execution flow
    Right now if you do  in the midst of continuation code
    running as the result of a load, control will return to the loader and context,
    and any load calls made after the  will not be processed in this run.
    
    These calls may potentially be lost, or another call to
    may be required.
    
    This means to find a solution we may need to  any Tasks that we did not make
    and continue past any Tasks that did (using  in ) method.
    I'm not sure if we can do this with a SynchronizationContext alone, or even with a custom TaskScheduler.
    Investigation is required on the TaskScheduler front, but we may need to resort to using a custom
    Awaitable type that notifies the loader when it has been awaited, at which point the loader can proceed.

---
+++ src/DataLoader/DataLoaderSynchronizationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataLoader
+{
+    internal class DataLoaderSynchronizationContext : SynchronizationContext
+    {
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} task {Task.CurrentId} - Post callback executing");
+            d(state);
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} task {Task.CurrentId} - Send callback executing");
+            d(state);
+        }
+    }
+}
