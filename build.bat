2de7f82c043f3b9049e87a654fa51c28ac1e6ef7
Daniel Zimmermann <daniel.zimmermann@serraview.com>
2017-01-29 14:27:12 +1100

    Allow running build.bat locally

--- build.bat
+++ build.bat
@@ -1,3 +1,15 @@
+if "%Configuration%" == "" (
+  if "%2" == "" (
+    set Configuration=Release
+  ) else (
+    set Configuration=%2
+  )
+)
+
+if "%PrereleaseTag%" == "" (
+  set PrereleaseTag=%1
+)
+
 dotnet restore || exit /b
 dotnet build src/DataLoader/ --configuration %Configuration% || exit /b
 dotnet test test/DataLoader.Tests/ --configuration %Configuration% || exit /b
