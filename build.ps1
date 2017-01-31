ef1683158a1adbc4062a1051a87dbfeca3db5ec3
Daniel Zimmermann <daniel.zimmermann@serraview.com>
2017-01-31 20:34:47 +1100

    Update to new .NET Core tooling

--- build.ps1
+++ build.ps1
@@ -7,13 +7,13 @@ param (
 dotnet restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-dotnet build src/DataLoader/ --configuration $Configuration
+dotnet build --configuration $Configuration
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-dotnet test test/DataLoader.Tests/ --configuration $Configuration
+dotnet test test/DataLoader.Tests/DataLoader.Tests.csproj --configuration $Configuration
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-dotnet pack src/DataLoader/ --configuration $Configuration --version-suffix $PrereleaseTag
+dotnet pack src/DataLoader/DataLoader.csproj --configuration $Configuration --include-symbols --version-suffix $PrereleaseTag
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 exit 0
