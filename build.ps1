386ceadc7af2091fb5a137c1f9f5e310d63f4b7b
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-02-01 16:02:05 +1100

    More build tweaks

--- build.ps1
+++ build.ps1
@@ -1,29 +1,28 @@
 param (
   [Parameter(Position = 1)]
   [string]$PrereleaseTag = $env:PrereleaseTag,
-  [string]$Configuration = "Release"
+  [string]$Configuration = "Release",
+  [switch]$SkipInstall
 )
 
 if (-not $PrereleaseTag) {
   $PrereleaseTag = "dev"
 }
 
-& .\dotnet-install.ps1
+if (-not $SkipInstall) {
+  & .\dotnet-install.ps1 -Architecture x64
+}
 
-dotnet restore
+dotnet restore DataLoader.sln
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-dotnet build .\src\DataLoader\ --configuration $Configuration
+dotnet build DataLoader.sln -configuration $Configuration
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-cd .\test\DataLoader.Tests\
-dotnet test --configuration $Configuration
+dotnet test test\DataLoader.Tests\DataLoader.Tests.csproj --configuration $Configuration
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-cd ..\..\
 
-cd .\src\DataLoader\
-dotnet pack --configuration $Configuration --include-symbols --version-suffix $PrereleaseTag
+dotnet pack src\DataLoader\DataLoader.csproj --configuration $Configuration --version-suffix $PrereleaseTag --include-symbols
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-cd ..\..\
 
 exit 0
